d76efeffbd10d27be3259e25b34571ffb4e684a8
Jamarri White <74019796+jamarricodesalot@users.noreply.github.com>
2022-03-28 22:09:12 -0400

    Update project1.s
    finished checkpoints. saving before proceeding to sums.

--- project1.s
+++ project1.s
@@ -8,27 +8,37 @@
         string: .asciiz "\n"
     .text
 main:
-    #reads input from user
-    li $v0, 8
-    la $a0, input
-    li $a1, 11
-    syscall
+   #reads input from user
+   li $v0, 8
+   la $a0, input
+   li $a1, 11
+   syscall
     
-    li $t0, 0
-    li $t1, 10
-    la $t2, input
-li $v0, 4
-la $a0, string
-syscall
-    
-    loop:
+   li $t0, 0
+   li $t1, 10
+   la $t2, input
+
+   loop:
             lb $a0, 0($t2)
             beq $a0, $t1, exit
             
             #checkpoints
             
             checkif09:
-                    li $t3. '0'
-                    blt $a0, $t3, invalidchar
-                    li $t3, '9'
-                    ble $a0, $t3, sum1
+                li $t3. '0'
+                blt $a0, $t3, invalidchar
+                li $t3, '9'
+                ble $a0, $t3, sum1
+           
+            checkifAT:
+                li $t3. 'A'
+                blt $a0, $t3, invalidchar
+                li $t3, 'T'
+                ble $a0, $t3, sum1
+                    
+                    
+            checkifAt:
+                li $t3. '0'
+                blt $a0, $t3, invalidchar
+                li $t3, '9'
+                ble $a0, $t3, sum1
